e9dae570d9c9279479e0509e219399541eea740b
kbuffardi <kbuffardi@csuchico.edu>
2018-12-05 18:57:41 +0000

    implementation of TTT tests

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -19,7 +19,11 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+  if(turn == X)
+    turn = O;
+  else
+    turn = X;
+  return turn;
 }
 
 /**
@@ -33,7 +37,18 @@ Piece TicTacToeBoard::toggleTurn()
 **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+  Piece tmp = getPiece(row, column);
+  if(tmp == Invalid)
+    return Invalid;
+  else if(tmp == Blank)
+  {
+    board[row][column] = turn;
+    tmp = turn;
+    toggleTurn();
+    return tmp;
+  }
+  else
+    return tmp;
 }
 
 /**
@@ -42,7 +57,13 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+  if(row>2 || column>2 || row <0 || column<0)
+    return Invalid;
+  else
+  {
+    Piece tmp = board[row][column];
+    return tmp;
+  }
 }
 
 /**
@@ -51,5 +72,22 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+  if(board[0][0] != ' ' && board[0][0] == board[0][1] && board[0][1] == board[0][2]) //top row
+    return board[0][0];
+  else if(board[1][0] != ' ' && board[1][0] == board[1][1] && board[1][1] == board[1][2]) //middle row
+    return board[1][0];
+  else if(board[2][0] != ' ' && board[2][0] == board[2][1] && board[2][1] == board[2][2]) //bottom row
+    return board[2][0];
+  else if(board[0][0] != ' ' && board[0][0] == board[1][0] && board[1][0] == board[2][0]) //left column
+    return board[0][0];
+  else if(board[0][1] != ' ' && board[0][1] == board[1][1] && board[1][1] == board[2][1]) //middle column
+    return board[0][1];  
+  else if(board[0][2] != ' ' && board[0][2] == board[1][2] && board[1][2] == board[2][2]) //right row
+    return board[0][2];
+  else if(board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) // diagonal
+    return board[0][0];
+  else if(board[2][0] != ' ' && board[2][0] == board[1][1] && board[1][1] == board[0][2]) // diagonal 2
+    return board[2][0];
+  else
+    return Invalid;
 }
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -14,9 +14,157 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-/* EXAMPLE TEST FORMAT
-TEST(TicTacToeBoardTest, unitTestName)
+TEST(TicTacToeBoardTest, toggle_testOnce)
 {
-	ASSERT_TRUE(true);
+	TicTacToeBoard board;
+	ASSERT_EQ(board.toggleTurn(), O);
 }
-*/
+
+TEST(TicTacToeBoardTest, toggle_testTwice)
+{
+	TicTacToeBoard board;
+	board.toggleTurn();
+	ASSERT_EQ(board.toggleTurn(), X);
+}
+
+TEST(TicTacToeBoardTest, placePiece_testOutBounds)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.placePiece(5,10), Invalid);
+}
+
+TEST(TicTacToeBoardTest, placePiece_testTaken)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,0);
+	ASSERT_EQ(board.placePiece(0,0), X);
+}
+
+TEST(TicTacToeBoard, getPiece_testBlank)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.getPiece(0,0), Blank);
+}
+
+TEST(TicTacToeBoardTest, getPiece_testX)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,0);
+	ASSERT_EQ(board.getPiece(0,0), X);
+}
+
+TEST(TicTacToeBoardTest, getPiece_testOutBounds)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.getPiece(5,10), Invalid);
+}
+
+
+TEST(TicTacToeBoardTest, getWinner_testTopRow)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,0);
+	board.placePiece(1,0);
+	board.placePiece(0,1);
+	board.placePiece(1,2);
+	board.placePiece(0,2);
+	ASSERT_EQ(board.getWinner(), X);
+}
+
+TEST(TicTacToeBoard, getWinner_testMiddleRow)
+{
+	TicTacToeBoard board;
+	board.placePiece(1,0);
+	board.toggleTurn();
+	board.placePiece(1,1);
+	board.toggleTurn();
+	board.placePiece(1,2);
+	ASSERT_EQ(board.getWinner(), X);
+}
+
+TEST(TicTacToeBoard, getWinner_testBottomRow)
+{
+	TicTacToeBoard board;
+	board.placePiece(2,0);
+	board.toggleTurn();
+	board.placePiece(2,1);
+	board.toggleTurn();
+	board.placePiece(2,2);
+	ASSERT_EQ(board.getWinner(), X);
+}
+
+TEST(TicTacToeBoard, getWinner_testLeftColumn)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,0);
+	board.toggleTurn();
+	board.placePiece(1,0);
+	board.toggleTurn();
+	board.placePiece(2,0);
+	ASSERT_EQ(board.getWinner(), X);
+}
+
+TEST(TicTacToeBoard, getWinner_testMiddleColumn)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,1);
+	board.toggleTurn();
+	board.placePiece(1,1);
+	board.toggleTurn();
+	board.placePiece(2,1);
+	ASSERT_EQ(board.getWinner(), X);
+}
+
+TEST(TicTacToeBoard, getWinner_testRightColumn)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,2);
+	board.toggleTurn();
+	board.placePiece(1,2);
+	board.toggleTurn();
+	board.placePiece(2,2);
+	ASSERT_EQ(board.getWinner(), X);
+}
+
+TEST(TicTacToeBoard, getWinner_testDiagonal1)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,0);
+	board.toggleTurn();
+	board.placePiece(1,1);
+	board.toggleTurn();
+	board.placePiece(2,2);
+	ASSERT_EQ(board.getWinner(), X);
+}
+
+TEST(TicTacToeBoard, getWinner_testDiagonal2)
+{
+	TicTacToeBoard board;
+	board.placePiece(0,2);
+	board.toggleTurn();
+	board.placePiece(1,1);
+	board.toggleTurn();
+	board.placePiece(2,0);
+	ASSERT_EQ(board.getWinner(), X);
+}
+
+TEST(TicTacToeBoard, getWinner_testInvalid)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.getWinner(), Invalid);
+}
+
+TEST(TicTacToeBoard, getWinner_testDraw)
+{
+	TicTacToeBoard board;
+	board.placePiece(2,0);
+	board.placePiece(0,0);
+	board.placePiece(0,1);
+	board.placePiece(1,0);
+	board.placePiece(1,1);
+	board.placePiece(2,1);
+	board.placePiece(1,2);
+	board.placePiece(0,2);
+	board.placePiece(2,2);
+	ASSERT_EQ(board.getWinner(), Blank);
+}
